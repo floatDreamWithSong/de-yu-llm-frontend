55578736e55b7347c2af7ff9b648fc567cf418b1
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-10-10 13:07:14 +0800

    feat: view Transition

--- src/main.tsx
+++ src/main.tsx
@@ -29,6 +29,7 @@ const router = createRouter({
   defaultPreload: "intent",
   scrollRestoration: true,
   defaultStructuralSharing: true,
+  defaultViewTransition: true,
   defaultPreloadStaleTime: 0,
 });
 
