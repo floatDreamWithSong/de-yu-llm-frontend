4fc42df7decec1270a4a5ca7f47e3cf1d52ed4aa
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-10-20 14:01:13 +0800

    feat: 添加移动端支持，优化AuthWrapper和AuthLayout组件以增强响应式设计

--- src/app/auth/layouts/AuthLayout.tsx
+++ src/app/auth/layouts/AuthLayout.tsx
@@ -1,11 +1,24 @@
 import { Outlet } from "@tanstack/react-router";
 import { useState } from "react";
 import { AuthContext } from "../context";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function AuthLayout() {
   const [phone, setPhone] = useState("");
   const [verificationCode, setverificationCode] = useState("");
   const [password, setPassword] = useState("");
+  const isMobile = useIsMobile()
+  if(isMobile) {
+    return <div>
+        <div className="space-y-4 text-center p-10">
+          <h1 className="text-4xl font-bold bg-gradient-to-r pb-2 from-[#594eff] via-[#8667f1] to-[#5a73fd] bg-clip-text text-transparent">
+            启创·InnoSpark
+          </h1>
+          <h2 className="text-[#5a5c72] text-xl">做有温度的教育大模型</h2>
+        </div>
+      <Outlet />
+    </div>
+  }
   return (
     <div
       className="min-h-screen bg-cover bg-center bg-no-repeat bg-fixed relative grid grid-cols-2"
